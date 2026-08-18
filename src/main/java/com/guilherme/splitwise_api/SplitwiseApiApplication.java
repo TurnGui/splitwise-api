a1242ccf018a33bafeff6629545ef644d00e25e1
TurnGui <guilhermefduarte2702@gmail.com>
2026-08-18 17:07:24 +0100

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/guilherme/splitwise_api/SplitwiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.splitwise_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/splitwise_api/SplitwiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.splitwise_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
