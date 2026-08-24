37dbe5bae80281dc80654b00cccf64028b64c0c7
TurnGui <guilhermefduarte2702@gmail.com>
2026-08-24 15:45:38 +0100

    feat: add user service and create user endpoint

---
+++ src/main/java/com/guilherme/splitwise_api/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.guilherme.splitwise_api.controller;
+
+import com.guilherme.splitwise_api.model.User;
+import com.guilherme.splitwise_api.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
---
+++ src/main/java/com/guilherme/splitwise_api/service/UserService.java
@@ -0,0 +1,19 @@
+package com.guilherme.splitwise_api.service;
+
+import com.guilherme.splitwise_api.model.User;
+import com.guilherme.splitwise_api.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
